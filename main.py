28695f67b33d02139cc69196ba9c756819fe5e3e
kourosh <kourosh.mahmoudi.2004@gmail.com>
2023-03-07 20:21:40 +0330

    adding data base part1

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-import sqlalchemy
+from flask_sqlalchemy import sqlalchemy
+
 #importing libraries needed 
     # flask
     # redirect to redirect a specific url to other
@@ -16,7 +17,21 @@ import sqlalchemy
 app = Flask(__name__)
 # declare sth in biggining
 app.secret_key = "hell"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["AQLALCHEMY_TRICK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(days=5)
+
+# initiallize the data base
+db = sqlalchemy(app) 
+
+class users(db.model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 # if the route is "sth" render sth
@@ -50,18 +65,30 @@ def logout():
         user = session["user"]
         flash(f"{user}  have been logged out!", "info")
         session.pop("user", None)
+        session.pop("email", None)
     return redirect(url_for("login"))
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html")
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved !")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template("user.html", email=email)
     else:
         flash("You are not logged in. Please login first !", "info")
         return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
